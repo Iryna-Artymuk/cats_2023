1c810b70768033003fe187d0c3fc45ccfe19c286
Iryna Artymuk <artimyk@ukr.net>
2023-09-25 16:31:03 +0300

    feat(creat gallery): creat gallry layout

--- src/pages/GalleryPage/GalleryPage.jsx
+++ src/pages/GalleryPage/GalleryPage.jsx
@@ -1,7 +1,22 @@
+import { useLocation } from 'react-router';
 import PageLayout from '../PageLayout/PageLayout';
+import { useRef } from 'react';
+import LinkBack from 'components/LinkBack/LinkBack';
+import PageName from 'components/PageName/PageName';
+import { StyledLinkBackWrapper } from './StyledGalleryPage';
 
 const GalleryPage = () => {
-  return <PageLayout pageName="Gallery"></PageLayout>;
+  const location = useLocation();
+  const backLinkHref = useRef(location.state?.from ?? '/');
+  return (
+    <PageLayout pageName="Gallery">
+      <StyledLinkBackWrapper>
+        <LinkBack backLinkHref={backLinkHref} />
+        <PageName name="Gallery" />
+      
+      </StyledLinkBackWrapper>
+    </PageLayout>
+  );
 };
 
 export default GalleryPage;
--- src/pages/GalleryPage/StyledGalleryPage.jsx
+++ src/pages/GalleryPage/StyledGalleryPage.jsx
@@ -1,11 +1,8 @@
 import { styled } from 'styled-components';
 
-export const Box = styled.div`
-  width: 100%;
-
-  outline: 2px solid blue;
-  @media screen and (min-width: 1440px) {
-    margin-left: 4.51%;
-    width: 47.22%;
-  }
+export const StyledLinkBackWrapper = styled.div`
+  display: flex;
+  gap: 10px;
 `;
+
+
